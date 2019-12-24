ae46b199f4985223fca3414ea32e4ad7c7a65198
JawherHamza <hamza.jawher@soprahr.com>
2019-12-24 10:49:01 +0100

    Structure

---
+++ src/containers/Employee/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmployeesPage from './containers/Employee/index'
+
+
+function App() {
+    return (
+        <div className="App">
+            <EmployeesPage />
+        </div>
+    );
+}
+
+export default App;
